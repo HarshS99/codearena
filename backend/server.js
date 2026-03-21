4b2ab1d31e02f52021a73c9ac243bedd5d111375
delta-demp <your-email@example.com>
2026-03-22 02:43:30 +0530

    fix gitignore and mongo connection

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,10 @@ app.get('/', (req, res) => {
 });
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("MongoDB connection error:", err));
 
